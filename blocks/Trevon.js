f7b459ba7a00e4709fc5139106a0d14026915d71
Hal Hockersmith <hal.hockersmith@gmail.com>
2018-01-25 02:19:15 -0600

    Changes by Trevon adding to his

--- blocks/Trevon.js
+++ blocks/Trevon.js
@@ -1,25 +1,41 @@
-var RunTimer, x, DriveSpeedL, DriveSpeedR;
+var RunTimer, x, DriveSpeedL, DriveSpeedR, _7BaccelerationVariable_7D, Timer, _7BelapsedTimeVariable_7D;
 
 /**
  * This function is executed when this Op Mode is selected from the Driver Station.
  */
 function runOpMode() {
-  RunTimer = elapsedTimeAccess.create_withResolution("SECONDS");
+  RunTimer = elapsedTimeAccess.create_withResolution("MILLISECONDS");
   InitMotors();
+  InitClaw();
   linearOpMode.waitForStart();
-  while (RunTimer < 10 && linearOpMode.opModeIsActive()) {
+  elapsedTimeAccess.reset(RunTimer);
+  while (RunTimer < 900 && linearOpMode.opModeIsActive()) {
     MotorSpeed(1, 1);
     telemetry.addTextData('RunTimer', String(RunTimer));
     telemetry.update();
   }
+  elapsedTimeAccess.reset(RunTimer);
+  while (RunTimer < 900 && linearOpMode.opModeIsActive()) {
+    MotorSpeed(0.25, -0.25);
+    telemetry.addTextData('RunTimer', String(RunTimer));
+    telemetry.update();
+  }
+  elapsedTimeAccess.reset(RunTimer);
+  while (RunTimer < 900 && linearOpMode.opModeIsActive()) {
+    MotorSpeed(0.25, 0.25);
+    telemetry.addTextData('RunTimer', String(RunTimer));
+    telemetry.update();
+  }
+  OpenClaw();
 }
 
 /**
  * Describe this function...
  */
-function MotorSpeed(DriveSpeedL, DriveSpeedR) {
-  frontrightdrive.setDualPower(DriveSpeedL, backrightdrive, DriveSpeedL);
-  frontleftdrive.setDualPower(DriveSpeedR, backleftdrive, DriveSpeedR);
+function InitClaw() {
+  rightclaw.setDirection("REVERSE");
+  rightclaw.setPosition(0.6);
+  leftclaw.setPosition(0.6);
 }
 
 /**
@@ -31,3 +47,32 @@ function InitMotors() {
   backleftdrive.setDirection("REVERSE");
   frontrightdrive.setDirection("REVERSE");
 }
+
+/**
+ * Describe this function...
+ */
+function OpenClaw() {
+  rightclaw.setPosition(0.64);
+  leftclaw.setPosition(0.64);
+}
+
+/**
+ * Describe this function...
+ */
+function CloseClaw() {
+  rightclaw.setPosition(0.4);
+  leftclaw.setPosition(0.4);
+}
+
+/**
+ * Describe this function...
+ */
+function MotorSpeed(DriveSpeedL, DriveSpeedR) {
+  frontrightdrive.setDualPower(DriveSpeedR, backrightdrive, DriveSpeedR);
+  frontleftdrive.setDualPower(DriveSpeedL, backleftdrive, DriveSpeedL);
+}
+
+
+elapsedTimeAccess.reset(RunTimer);
+
+elapsedTimeAccess.create_withResolution("SECONDS");
