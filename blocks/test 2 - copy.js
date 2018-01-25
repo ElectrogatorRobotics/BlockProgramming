04677733b4d8862bf445aa01ce251d827cb0d3c2
Hal Hockersmith <hal.hockersmith@gmail.com>
2018-01-25 02:23:42 -0600

    Mira's additions to her block mode.

--- blocks/test 2 - copy.js
+++ blocks/test 2 - copy.js
@@ -1,17 +1,17 @@
-var RunTimer, x, DriveSpeedL, DriveSpeedR;
+var RunTimer, x, DriveSpeedL, DriveSpeedR, LiftSpeed;
 
 /**
  * This function is executed when this Op Mode is selected from the Driver Station.
  */
 function runOpMode() {
   RunTimer = elapsedTimeAccess.create_withResolution("SECONDS");
-  InitMotors();
+  Initdrivemotor();
   linearOpMode.waitForStart();
   while (RunTimer < 10 && linearOpMode.opModeIsActive()) {
     MotorSpeed(1, 1);
-    telemetry.addTextData('RunTimer', String(RunTimer));
-    telemetry.update();
   }
+  telemetry.addTextData('RunTimer', String(RunTimer));
+  telemetry.update();
 }
 
 /**
@@ -25,7 +25,25 @@ function MotorSpeed(DriveSpeedL, DriveSpeedR) {
 /**
  * Describe this function...
  */
-function InitMotors() {
+function motorlifter(LiftSpeed) {
+  liftmotor1.setDualPower(LiftSpeed, liftmotor1, LiftSpeed);
+  liftmotor1.setDirection("FORWARD");
+  liftmotor2.setDualPower(LiftSpeed, liftmotor2, LiftSpeed);
+  liftmotor1.setDirection("FORWARD");
+}
+
+/**
+ * Describe this function...
+ */
+function Initliftermotor() {
+  liftmotor1.setDualMode("RUN_USING_ENCODER", liftmotor2, "RUN_USING_ENCODER");
+  liftmotor2.setDirection("REVERSE");
+}
+
+/**
+ * Describe this function...
+ */
+function Initdrivemotor() {
   frontrightdrive.setDualMode("RUN_USING_ENCODER", frontleftdrive, "RUN_USING_ENCODER");
   backleftdrive.setDualMode("RUN_USING_ENCODER", backrightdrive, "RUN_USING_ENCODER");
   backrightdrive.setDirection("REVERSE");
